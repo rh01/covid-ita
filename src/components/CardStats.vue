f4b77ea43073e84eab656d3c67f90e79fbfb3b1a
asdf1899 <araid.anas99@gmail.com>
2020-04-06 02:16:55 +0200

    Add axios and moment

--- src/components/CardStats.vue
+++ src/components/CardStats.vue
@@ -1,39 +1,73 @@
 <template>
-    <section class="section">
-        <div class="container">
-            <h4  class="has-text-centered is-size-4">Ultimo aggiornamento: </h4><br>          
-            <div class="tile is-ancestor" style="text-align:center">
-                <div class="tile is-parent">
-                    <article class="tile is-child box style-box-grey">
-                        <p class="subtitle">Casi totali</p>
-                        <p class="title has-text-dark">0</p>
-                    </article>
-                </div>
-                <div class="tile is-parent">
-                    <article class="tile is-child box style-box-yellow">
-                        <p class="subtitle">Positivi</p>
-                        <p class="title has-text-warning">0</p>
-                    </article>
-                </div>
-                <div class="tile is-parent">
-                    <article class="tile is-child box style-box-red">
-                        <p class="subtitle">Deceduti</p>
-                        <p class="title has-text-danger">0</p>
-                    </article>
-                </div>
-                <div class="tile is-parent">
-                    <article class="tile is-child box style-box-green">
-                        <p class="subtitle ì">Guariti</p>
-                        <p class="title has-text-success">0</p>
-                    </article>
+    <div>
+        <h4 class="has-text-centered is-size-4">Ultimo aggiornamento: {{ this.dataAggiornamento }}</h4><br>          
+        <section class="section">
+            <div class="container">
+                <div class="tile is-ancestor" style="text-align:center">
+                    <div class="tile is-parent">
+                        <article class="tile is-child box style-box-grey">
+                            <p class="subtitle">Casi totali</p>
+                            <p class="title has-text-dark">{{ this.casi }}</p>
+                        </article>
+                    </div>
+                    <div class="tile is-parent">
+                        <article class="tile is-child box style-box-yellow">
+                            <p class="subtitle">Positivi</p>
+                            <p class="title has-text-warning">{{ this.positivi }}</p>
+                        </article>
+                    </div>
+                    <div class="tile is-parent">
+                        <article class="tile is-child box style-box-red">
+                            <p class="subtitle">Deceduti</p>
+                            <p class="title has-text-danger">{{ this.deceduti }}</p>
+                        </article>
+                    </div>
+                    <div class="tile is-parent">
+                        <article class="tile is-child box style-box-green">
+                            <p class="subtitle ì">Guariti</p>
+                            <p class="title has-text-success">{{ this.guariti }}</p>
+                        </article>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 </template>
 
 <script>
+    import axios from 'axios';
+    import moment from 'moment';
+
     export default {
-        name: 'CardStats'
+        name: 'CardStats',
+        data: function() {
+            return {
+                loading: true,
+                errored: false,
+                info: null,
+                guariti: 0,
+                casi: 0,
+                deceduti: 0,
+                positivi: 0,
+                dataAggiornamento: null
+            }
+        },
+        mounted () {
+            axios
+            .get('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json')
+            .then(response => {
+                this.info = response.data;
+                this.positivi = this.info[0].nuovi_positivi;
+                this.guariti = this.info[0].dimessi_guariti;
+                this.deceduti = this.info[0].deceduti;
+                this.casi = this.info[0].totale_casi;
+                this.dataAggiornamento = moment(this.info[0].data).format('MM/DD/YYYY');
+            })
+            .catch(error => {
+                console.log(error);
+                this.errored = true;
+            })
+            .finally(() => this.loading = false)
+        }
     }
 </script>
